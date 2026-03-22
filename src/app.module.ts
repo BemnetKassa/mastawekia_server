37130b067e98cbf3e50a53288cfd98df089b299d
Bemnet kassa Bekele <bemnetkassa79@gmail.com>
2026-03-22 19:56:16 +0300

    here we go server

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+// ...existing code...
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+
+@Module({
+  imports: [AuthModule, UserModule, PrismaModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
