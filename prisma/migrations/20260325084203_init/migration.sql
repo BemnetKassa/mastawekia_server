e0b9c40bc19399741465ab2f864aa2347682849f
Bemnet kassa Bekele <bemnetkassa79@gmail.com>
2026-03-25 11:44:51 +0300

    fixing prisma migration error
    i have fixed the prisma error and other version errors by reinstalling the prisma restoring the prisma migration

--- prisma/migrations/20260325084203_init/migration.sql
+++ prisma/migrations/20260325084203_init/migration.sql
@@ -1,3 +1,17 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'CLIENT');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'USER',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateTable
 CREATE TABLE "JobPost" (
     "id" TEXT NOT NULL,
@@ -9,6 +23,9 @@ CREATE TABLE "JobPost" (
 
     CONSTRAINT "JobPost_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- AddForeignKey
 ALTER TABLE "JobPost" ADD CONSTRAINT "JobPost_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
